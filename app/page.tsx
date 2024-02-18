40dd2f9b6c7eaa36b85f2f349f79999f4ced4f24
Benazir Mulla <mullabenazir@yahoo.com>
2024-02-18 21:31:23 +0530

    added header file as common

--- app/page.tsx
+++ app/page.tsx
@@ -1,161 +1,52 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 import Link from "next/link";
 import jsonData from "@/public/locales/en/common";
-// const Item = styled(Paper)(({ theme }) => ({
-//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//   ...theme.typography.body2,
-//   padding: theme.spacing(1),
-//   textAlign: 'center',
-//   color: theme.palette.text.secondary,
-// }));
+import Header from "./components/Header";
 
 export default function Home() {
   const t = jsonData;
+
+
   return (
     <>
-      <header style={{ padding: '.5rem 2rem', display:'flex', alignItems:'center',justifyContent:'space-between', borderBottom:'1px solid #E5EAF2' }}>
-
-        <Image height={50} width={140} src={`/logo.png`} alt={"logo"} />
-
-        <nav aria-label="main mailbox folders" style={{width:'30%'}}>
-
-          <ul style={{ listStyleType: 'none', display: 'flex', gap: '1.5rem', fontSize: '1.1rem' }}>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">Blog</Link>
-            </li>
-            <li>
-              <Link href="/blog/hello-world">Gallery</Link>
-            </li>
-            <li>
-              <Link href="/blog/hello-world">Contact Us</Link>
-            </li>
-          </ul>
-
-          {/* <List style={{ display: "flex" }} >
-                <ListItem>
-                  <ListItemButton >
-                    <ListItemText primary={<Typography variant="h6">
-                      HOME
-                    </Typography>} />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem>
-                  <ListItemButton>
-                    <ListItemText primary={<Typography variant="h6">
-                      BLOG
-                    </Typography>} />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem >
-                  <ListItemButton>
-                    <ListItemText primary={<Typography variant="h6">
-                      GALLERY
-                    </Typography>} />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem >
-                  <ListItemButton>
-                    <ListItemText primary={<Typography variant="h6">
-                      CONTACT US
-                    </Typography>} />
-                  </ListItemButton>
-                </ListItem>
-              </List> */}
-        </nav>
-
-      </header>
+  <Header></Header>
       <main className={styles.main}>
-        <div >
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 3, sm: 4, md: 5 }}>
-
+        <div>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 3, sm: 4, md: 5 }}
+          >
             <Grid item xs={6}>
               <p>
-                <Typography variant="h5" >
-                 {t.line1}
-                </Typography>
-                <Typography variant="h5">
-                 {t.line2}
-                </Typography>
+                <Typography variant="h5">{t.line1}</Typography>
+                <Typography variant="h5">{t.line2}</Typography>
               </p>
             </Grid>
             <Grid item xs={6}>
-              <Image height={380} width={850} src={process.env.NEXT_PUBLIC_BASE_URL + `/assets/insurance-Broker-vs-Insurer.jpg`} alt={"logo"} />
+              <Image
+                height={380}
+                width={850}
+                src={
+                  process.env.NEXT_PUBLIC_BASE_URL +
+                  `/assets/insurance-Broker-vs-Insurer.jpg`
+                }
+                alt={"logo"}
+              />
             </Grid>
           </Grid>
         </div>
-
-        {/* <div className={styles.description}>
-        <div>
-          <p>
-            <Typography variant="h1" gutterBottom>
-              LegalNex HUB
-            </Typography>
-            VERIFY WITH CONFIDENCE
-          </p> */}
-
-        {/* <div>
-        <p>
-          <Typography variant="h6" gutterBottom>
-          The world is changing faster than ever and for good. With the ever increasing documentation requirements,
-enhancements in regulatory mandates, updates in the legal procedures, amendments in laws, technological
-upgrades, changes in the outlook of the judicial system, exponentially increased fraud landscape and
-every-legal-thing around; it is becoming extremely challenging for businesses to keep up with the pace of the
-change.
-            </Typography>
-          </p>
-        </div> */}
-
-        {/* </div>
-        <div>
-          <nav aria-label="main mailbox folders">
-            <List style={{ display: "flex" }}>
-              <ListItem >
-                <ListItemButton style={{ width: "200px", textAlign: "center" }}>
-                  <ListItemText primary={<Typography variant="h5" gutterBottom>
-                    HOME
-                  </Typography>} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem >
-                <ListItemButton style={{ width: "200px", textAlign: "center" }}>
-                  <ListItemText primary={<Typography variant="h5" gutterBottom>
-                    BLOG
-                  </Typography>} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem >
-                <ListItemButton style={{ width: "200px", textAlign: "center" }}>
-                  <ListItemText primary={<Typography variant="h5" gutterBottom>
-                    GALLERY
-                  </Typography>} />
-                </ListItemButton>
-              </ListItem>
-              <ListItem >
-                <ListItemButton style={{ width: "200px", textAlign: "center" }}>
-                  <ListItemText primary={<Typography variant="h5" gutterBottom>
-                    CONTACT US
-                  </Typography>} />
-                </ListItemButton>
-              </ListItem>
-            </List>
-            <Divider style={{ borderColor: "#add8e6" }} />
-          </nav>
-        </div>
-      </div> */}
       </main>
     </>
   );
